c5366bbbc82c9e59b608a21de7c317c33fe25f9c
amanshcode <amam04sharma@gmail.com>
2026-06-16 11:04:26 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,40 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
+const cors = require('cors');
 const connectDB = require('./config/db');
-connectDB();
-const userRoutes = require('./routes/authRoutes');
-const path = require("path");
+const path = require('path');
 
+dotenv.config();
+connectDB();
 
 const app = express();
-app.use(cors(
-  {
-    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
-    credentials: true,
-  }
-));
+
+// Set CORS for frontend URL / allow single-node deploy
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
-app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('ShopNest API is running in Development mode...');
+  });
+}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
-const PORT = process.env.PORT || 3000;
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
